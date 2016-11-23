45d1c7eecb4cb27085114c12e60988df0599f39f
unknown <santosh2504@gmail.com>
2016-11-23 11:57:50 +0530

    initial commit

---
+++ pages/index.php
@@ -0,0 +1,76 @@
+<?php include"header.php"?>
+    <!-- Hero Image section  -->
+    <div class="jumbotron">
+        <section class="container">
+            <section class="contactus">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="panel panel-primary contactpanel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Contact Us</h3> </div>
+                            <div class="panel-body">
+                                <form role="form" action="" method="post">
+                                    <div class="col-lg-12">
+
+                                        <div class="form-group">
+                                            <label for="InputName">Your Name</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required><span class="input-group-addon"><i class="glyphicon glyphicon-asterisk form-control-feedback"></i></span></div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="InputEmail">Your Email</label>
+                                            <div class="input-group">
+                                                <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required> <span class="input-group-addon"><i class="glyphicon glyphicon-asterisk form-control-feedback"></i></span></div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="InputMessage">Message</label>
+                                            <div class="input-group">
+                                                <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea> <span class="input-group-addon"><i class="glyphicon glyphicon-asterisk form-control-feedback"></i></span></div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="InputReal">What is 4+3? (Simple Spam Checker)</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" name="InputReal" id="InputReal" required> <span class="input-group-addon"><i class="glyphicon glyphicon-asterisk form-control-feedback"></i></span></div>
+                                        </div>
+                                        <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right"> </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-md-offset-2 schoolname">
+                    <h1>Welcome to</h1>
+                    <h1>Dr Besent School</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime eos incidunt dolorem reiciendis alias obcaecati, voluptates, perspiciatis provident. Odit deleniti modi eveniet quidem dolorum error sit placeat, vitae quo culpa.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut minus itaque, cum consequatur laboriosam magni illum repellat, eum asperiores ea aspernatur odit error quos fugit fugiat ipsum dignissimos facilis deserunt.</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+    </div>
+    <!--    section navbar bottom -->
+    <nav role="navigation" class="navbar navbar-inverse navbar-bottom">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a href="#" class="navbar-brand">Dr Besant School</a> </div>
+            <!-- Collection of nav links and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Our School</a></li>
+                    <li><a href="#">Addmissions</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Employement</a></li>
+                    <li><a href="#">Store</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--section about school-->
+    <section class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus fugiat nemo, odit porro voluptas, molestias aperiam, quos suscipit iusto inventore adipisci saepe veritatis aut obcaecati ex eius mollitia at earum!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio animi doloremque minus delectus necessitatibus quo, maiores neque minima. Itaque soluta molestias reprehenderit nesciunt consectetur blanditiis. Obcaecati error, sunt deserunt quaerat.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium delectus quis, cupiditate voluptatibus odit, facere iure? Veritatis eius ipsam quae magnam sed sapiente tenetur, omnis, quidem iusto eum quo vitae!</p>
+    </section>
+    <?php include"footer.php"?>
